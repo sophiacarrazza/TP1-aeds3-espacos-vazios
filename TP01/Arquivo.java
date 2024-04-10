019d7794ad44ed7c39fa61f10c14218b811237d1
Sophia Carrazza <sophiacarrazza7@gmail.com>
2024-04-10 20:26:29 -0300

    alteracoes no delete e no create - simplificacao

--- TP01/Arquivo.java
+++ TP01/Arquivo.java
@@ -7,14 +7,14 @@ public class Arquivo<T extends Registro> {
 
   protected RandomAccessFile arquivo;
   protected Constructor<T> construtor;
-  
+
   String nomeArquivo = "";
   final protected int TAM_CABECALHO = 4;
 
-  public Arquivo(String filmes , Constructor<T> c) throws Exception {
+  public Arquivo(String filmes, Constructor<T> c) throws Exception {
     this.nomeArquivo = filmes;
     this.construtor = c;
-    
+
     arquivo = new RandomAccessFile(filmes, "rw");
     if (arquivo.length() < TAM_CABECALHO) {
       arquivo.seek(0);
@@ -24,77 +24,41 @@ public class Arquivo<T extends Registro> {
 
   public int create(T obj) throws Exception {
     arquivo.seek(0);
-    int ultimoID = arquivo.readInt();
-    ultimoID++;
-    arquivo.seek(0);
-    arquivo.writeInt(ultimoID);
-    obj.setId(ultimoID);
-
     byte[] ba = obj.toByteArray();
     int tam = ba.length;
-    
-          arquivo.seek(arquivo.length());
-        // long endereco = arquivo.getFilePointer();
-          
-          arquivo.write(' '); // lápide
-          arquivo.writeInt(tam);
-          
-          arquivo.write(ba);
-        
-        
-        
-    
+
+    arquivo.seek(TAM_CABECALHO);
+    while (arquivo.getFilePointer() < arquivo.length()) {
+      byte lapide = arquivo.readByte();
+      int tamanho = arquivo.readInt();
+      if (lapide == '*' && tamanho <= tam) {
+        arquivo.write(' ');
+        arquivo.writeInt(tam);
+        arquivo.write(ba);
         return obj.getId();
       }
-
-  public boolean Update(T obj){
-    
-    int id = obj.getId();
-    try{
-      arquivo.seek(TAM_CABECALHO);
-      while(arquivo.getFilePointer() < arquivo.length()){
-
-        byte[] ba = obj.toByteArray();
-        int objTam = ba.length;
-
-
-        long lapide = arquivo.getFilePointer();
-        byte lapideValor = arquivo.readByte();
-        int tamanhoRegistro = arquivo.readInt();
-        
-        int idAtual = arquivo.readInt();
-        
-
-        if(idAtual == id && lapideValor == ' '){
-          arquivo.seek(lapide);
-
-          if(tamanhoRegistro >= objTam){
-
-              arquivo.writeByte(' ');
-              arquivo.writeInt(objTam);
-              arquivo.write(ba);
-
-          } else {
-            arquivo.seek(lapide);
-            arquivo.writeByte('*');
-            arquivo.seek(arquivo.length());
-            arquivo.writeByte(' ');
-            arquivo.writeInt(objTam);
-            arquivo.write(ba);
-          }
-          return true;
-        } else{
-          arquivo.skipBytes(tamanhoRegistro - 4);
-        }
-      }
-
-    } catch (Exception e){
-      System.out.println("Ocorreu uma excessao: "+ e);
-      
+      arquivo.skipBytes(tamanho);
     }
-    return false;
-    
+    if (arquivo.getFilePointer() == arquivo.length()) {
+      arquivo.write(' ');
+      arquivo.writeInt(tam);
+      arquivo.write(ba);
+    }
+    return obj.getId();
   }
+
+  public boolean Update(T obj) {
+    try {
+      delete(obj.getId());
+      create(obj);
+
+    } catch (Exception e) {
+      System.out.println("Erro ao fechar o arquivo: " + e.getMessage());
+      return false;
+    }
+    return true;
+  }
+
   public T read(int id) throws Exception {
     T l = construtor.newInstance();
     byte[] ba;
@@ -102,7 +66,6 @@ public class Arquivo<T extends Registro> {
     byte lapide;
     arquivo.seek(TAM_CABECALHO);
     while (arquivo.getFilePointer() < arquivo.length()) {
-
 
       lapide = arquivo.readByte();
       tam = arquivo.readInt();
@@ -118,17 +81,19 @@ public class Arquivo<T extends Registro> {
     }
     return null;
   }
+
   public boolean delete(int id) throws Exception {
-        long endereco =getenderecoid(id);
-        arquivo.seek(endereco);
-        if (endereco!=-1){
-            arquivo.write('*');              
-            return true;
-        }
+    long endereco = getenderecoid(id);
+    arquivo.seek(endereco);
+    if (endereco != -1) {
+      arquivo.write('*');
+      return true;
+    }
     return false;
-}
-    private long getenderecoid(int id) throws Exception{
-        T l = construtor.newInstance();
+  }
+
+  private long getenderecoid(int id) throws Exception {
+    T l = construtor.newInstance();
     byte[] ba;
     int tam;
     byte lapide;
@@ -136,24 +101,24 @@ public class Arquivo<T extends Registro> {
 
     arquivo.seek(TAM_CABECALHO);
     while (arquivo.getFilePointer() < arquivo.length()) {
-        endereco = arquivo.getFilePointer();
-        lapide = arquivo.readByte();
-        tam = arquivo.readInt();
-        
-        if (lapide == ' ') {
-            ba = new byte[tam];
-            arquivo.read(ba);
-            l.fromByteArray(ba);
-            if (l.getId() == id) {
-                
-                return endereco;
-            }
-        } else {
-            arquivo.skipBytes(tam);
+      endereco = arquivo.getFilePointer();
+      lapide = arquivo.readByte();
+      tam = arquivo.readInt();
+
+      if (lapide == ' ') {
+        ba = new byte[tam];
+        arquivo.read(ba);
+        l.fromByteArray(ba);
+        if (l.getId() == id) {
+
+          return endereco;
         }
+      } else {
+        arquivo.skipBytes(tam);
+      }
     }
     return (long) -1;
-    }
+  }
 
   public void close() {
     try {
@@ -164,7 +129,5 @@ public class Arquivo<T extends Registro> {
       System.out.println("Erro ao fechar o arquivo: " + e.getMessage());
     }
   }
-
-  
 
 }
--- TP01/Principal.java
+++ TP01/Principal.java
@@ -85,61 +85,62 @@ public class Principal {
             // UPDATE
 
             // // Seção: Atualização de Filme 1
-            /*livro5.setNome("Interestelar");
-             if (arquivoFilmes.update(livro5))
-                 System.out.println("Filme de ID " + livro5.getId() + " alterado para: " +
-             livro5.getNome());
-             else
-              System.out.println("Não foi possível atualizar o filme de ID " +
-             livro5.getId());
-            
-            // // Seção: Atualização de Filme 2
-             livro2.setNome("AntArestral");
-             if (arquivoFilmes.update(livro2))
-             System.out.println("Filme de ID " + livro2.getId() + " alterado para: " +
-             livro2.getNome());
-             else
-             System.out.println("Não foi possível atualizar o filme de ID " +
-             livro2.getId());
-            /* 
-            // // Seção: Atualização de Filme 3
-             livro3.setNome("Flores");
-             if (arquivoFilmes.update(livro3))
-             System.out.println("Filme de ID " + livro3.getId() + " alterado para: " +
-             livro3.getNome());
-             else
-             System.out.println("Não foi possível atualizar o filme de ID " +
-             livro3.getId());
-            
-            // // Seção: Atualização de Filme 4
-             livro4.setNome("Hulk");
-             if (arquivoFilmes.update(livro4))
-             System.out.println("Filme de ID " + livro4.getId() + " alterado para: " +
-             livro4.getNome());
-             else
-             System.out.println("Não foi possível atualizar o filme de ID " +
-             livro4.getId());
-            
-            // // Seção: Atualização de Filme 5
-             livro1.setNome("Barbie and Dreamhouse");
-             if (arquivoFilmes.update(livro1))
-             System.out.println("Filme de ID " + livro1.getId() + " alterado para: " +
-             livro1.getNome());
-             else
-             System.out.println("Não foi possível atualizar o filme de ID " +
-             livro1.getId());
-            
-            // // Seção: Atualização de Filme 6
-             livro6.setNome("Branca de neve");
-             if (arquivoFilmes.update(livro6))
-             System.out.println("Filme de ID " + livro6.getId() + " alterado para: " +
-             livro6.getNome());
-             else
-             System.out.println("Não foi possível atualizar o filme de ID " +
-             livro6.getId());
-            */
+            /*
+             * livro5.setNome("Interestelar");
+             * if (arquivoFilmes.update(livro5))
+             * System.out.println("Filme de ID " + livro5.getId() + " alterado para: " +
+             * livro5.getNome());
+             * else
+             * System.out.println("Não foi possível atualizar o filme de ID " +
+             * livro5.getId());
+             * 
+             * // // Seção: Atualização de Filme 2
+             * livro2.setNome("AntArestral");
+             * if (arquivoFilmes.update(livro2))
+             * System.out.println("Filme de ID " + livro2.getId() + " alterado para: " +
+             * livro2.getNome());
+             * else
+             * System.out.println("Não foi possível atualizar o filme de ID " +
+             * livro2.getId());
+             * /*
+             * // // Seção: Atualização de Filme 3
+             * livro3.setNome("Flores");
+             * if (arquivoFilmes.update(livro3))
+             * System.out.println("Filme de ID " + livro3.getId() + " alterado para: " +
+             * livro3.getNome());
+             * else
+             * System.out.println("Não foi possível atualizar o filme de ID " +
+             * livro3.getId());
+             * 
+             * // // Seção: Atualização de Filme 4
+             * livro4.setNome("Hulk");
+             * if (arquivoFilmes.update(livro4))
+             * System.out.println("Filme de ID " + livro4.getId() + " alterado para: " +
+             * livro4.getNome());
+             * else
+             * System.out.println("Não foi possível atualizar o filme de ID " +
+             * livro4.getId());
+             * 
+             * // // Seção: Atualização de Filme 5
+             * livro1.setNome("Barbie and Dreamhouse");
+             * if (arquivoFilmes.update(livro1))
+             * System.out.println("Filme de ID " + livro1.getId() + " alterado para: " +
+             * livro1.getNome());
+             * else
+             * System.out.println("Não foi possível atualizar o filme de ID " +
+             * livro1.getId());
+             * 
+             * // // Seção: Atualização de Filme 6
+             * livro6.setNome("Branca de neve");
+             * if (arquivoFilmes.update(livro6))
+             * System.out.println("Filme de ID " + livro6.getId() + " alterado para: " +
+             * livro6.getNome());
+             * else
+             * System.out.println("Não foi possível atualizar o filme de ID " +
+             * livro6.getId());
+             */
             // DELETE
-            /* 
+
             // Seção: Exclusão de Filme 1
             if (arquivoFilmes.delete(livro1.getId()))
                 System.out.println("Filme \"" + livro1.getNome() + "\" de ID " + idLivro1 + " excluído!");
@@ -181,8 +182,7 @@ public class Principal {
             else
                 System.out
                         .println("Filme \"" + livro6.getNome() + "\" de ID " + idLivro6 + " não foi possível excluir!");
-            */
-        
+
             // FECHANDO ARQUIVO
             arquivoFilmes.close();
 
